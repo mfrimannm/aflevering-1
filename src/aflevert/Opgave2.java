044676670faa1b59d77aff52116e84c2ed4aae63
Mads Frimann Madsen <mfrimannm@gmail.com>
2014-02-25 08:54:04 +0100

    hh

--- src/aflevert/Opgave2.java
+++ src/aflevert/Opgave2.java
@@ -1,8 +1,8 @@
 package aflevert;
 
 public class Opgave2 {
-public static void main(String[] args) {
-double x= 7.2;
-System.out.println(1 + 3 + x + x + x + x + x + 5 / 4 * 2);
+	public static void main(String[] args) {
+		double x = 7.2;
+		System.out.println(1 + 3 + x + x + x + x + x + 5 / 4 * 2);
 	}
 }
--- src/aflevert/Opgave3.java
+++ src/aflevert/Opgave3.java
@@ -1,14 +1,15 @@
 package aflevert;
 
 public class Opgave3 {
-public static final int LINES = 3;
-public static void main(String[] args) {
-for (int i = 1; i <= LINES ; i++) {
-	System.out.println();
-for (int j = 1; j <= LINES; j++) {
-System.out.print("+-");
-						}
-System.out.print("+");
-					}
+	public static final int LINES = 3;
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= LINES; i++) {
+			System.out.println();
+			for (int j = 1; j <= LINES; j++) {
+				System.out.print("+-");
 			}
+			System.out.print("+");
 		}
+	}
+}
