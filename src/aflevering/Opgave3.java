60b4166d7c07a9f142c8416ba82a7d201612d4f6
Mads Frimann Madsen <mfrimannm@gmail.com>
2014-02-25 17:50:47 +0100

    gjort pænere

--- src/aflevering/Opgave3.java
+++ src/aflevering/Opgave3.java
@@ -1,9 +1,9 @@
 package aflevering;
 
 public class Opgave3 {
-	
+
 	public static final int LINES = 3;
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		program();
@@ -11,16 +11,20 @@ public class Opgave3 {
 
 	private static void program() {
 		// TODO Auto-generated method stub
-		for(int k=0;k<=LINES;k++){
-			System.out.println("Først skrives der ud med " + k + " linjer, derefter stiger k med én og man kører programmet igen");  //egen kode
-			for (int i = 1; i <= k ; i++) {
+		for (int k = 0; k <= LINES; k++) {
+			System.out
+					.println("Først skrives der ud med "
+							+ k
+							+ " linjer, derefter stiger k med én og man kører programmet igen"); // egen
+																									// kode
+			for (int i = 1; i <= k; i++) {
 				for (int j = 1; j <= k; j++) {
 					System.out.print("+-");
-			}
+				}
 				System.out.println("+"); // rykket
-}
-			System.out.println(); //egen kode
-			
-}
-}
+			}
+			System.out.println(); // egen kode
+
+		}
+	}
 }
